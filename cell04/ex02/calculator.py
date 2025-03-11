521f0a5a8ee3c8d59b46ebc2b3fe9f4cbba820f9
Sukrit834 <40691@prot.ac.th>
2025-03-11 11:58:02 +0700

    Delete cell04/ex02 directory

--- cell04/ex02/calculator.py
+++
@@ -1,9 +0,0 @@
-A = int(input("Give me the first number: "))
-B = int(input("Give me the second number: "))
-
-print("Thank you!")
-
-C = ["+","-","/","*"]
-
-for i in range(1,5):
-  print(f"{A} {C[i-1]} {B} = {eval(f'{A} {C[i-1]} {B}')}")
